fb274c0e4339ca0eef91b641f32f98d040187cdc
vladamljc <vladimir.milijic@logeecom.com>
2019-12-06 12:17:11 +0100

    Fix code style issues in MiddlewarePipeline.php
    ISSUE DEMOVM-10

--- src/Middleware/MiddlewarePipeline.php
+++ src/Middleware/MiddlewarePipeline.php
@@ -20,16 +20,17 @@ class MiddlewarePipeline
     }
 
     /**
+     * Checks if request can pass trough the list of middlewares.
+     *
      * @param Request $request
      * @param array $listMiddlewareNames
      */
     public static function process(Request $request, array $listMiddlewareNames): void
     {
         foreach ($listMiddlewareNames as $middlewareName) {
+            /** @var Middleware $middleware */
             $middleware = new $middlewareName;
-            echo "Middleware {$middlewareName} constructed";
             $middleware->handle($request);
-            echo "Middleware {$middlewareName} handle method called";
         }
     }
 
