9e517478cd02b3d46cb8be5684fc7ca8453ed4b3
vladamljc <vladimir.milijic@logeecom.com>
2019-12-09 07:47:09 +0100

    Fix code style issues in routes.php
    ISSUE DEMOVM-10

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,5 @@ use Catalog\Controllers\HomeController;
 use Catalog\Routes\Route;
 use Catalog\Routes\Routes;
 
-$route2 = new Route(HomeController::class, 'renderHomePage', '/', 'GET');
+Routes::add(new Route(HomeController::class, 'renderHomePage', '/', 'GET'));
 
-Routes::add($route2);
